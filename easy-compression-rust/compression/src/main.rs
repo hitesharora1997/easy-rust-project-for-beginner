fed0ef3188d85e3ac99392f60d329c44066f6bc8
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-18 13:38:58 +0200

    Made some changes

--- easy-compression-rust/compression/src/main.rs
+++ easy-compression-rust/compression/src/main.rs
@@ -6,17 +6,21 @@ use std::env::args; // to accept the command line arguments
 use std::fs::File; // since we are working with the files
 use std::io::copy; // to copy the file
 use std::io::BufReader; // to read the files
-use std::time::Instant; // here it will show how much time taken to compress the files 
+use std::time::Instant; // here it will show how much time taken to compress the files
 
 fn main() {
-    if args().len() != 3 { // if you give less than 3 arg the program will get panic
-        eprintln!("Usage: 'source' 'target' "); // we use `eprintln` instead of `println` as eprintln is only for error and progress message. 
+    if args().len() != 3 {
+        // if you give less than 3 arg the program will get panic
+        eprintln!("Usage: 'source' 'target'"); // we use `eprintln` instead of `println` as eprintln is only for error and progress message.
         return;
     }
-    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
-    let output = File::create(args().net(2).unwrap()).unwrap();
-    let mut encoder = GzEncoder::new(output, Compression::default());
-    let start = Instant::now();
-    let output = encoder
 
+    // let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    // let output = File::create(args().net(2).unwrap()).unwrap();
+    // let mut encoder = GzEncoder::new(output, Compression::default());
+    // let start = Instant::now();
+    // let output = encoder
+
+    let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
+    let output = File::create(args().nth(2).unwrap()).unwrap();
 }
