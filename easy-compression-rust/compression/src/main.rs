388d2345c1dbe65e1a697eddf30856db5a3b87e3
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-18 13:10:11 +0200

    Made some changes

--- easy-compression-rust/compression/src/main.rs
+++ easy-compression-rust/compression/src/main.rs
@@ -1,6 +1,6 @@
-extern crate flate2; //import the flate2 package
+extern crate flate2; // import the `flate2` package
 
-use flate2::write::GzEncoder; // from flate2 package we are using this encoder
+use flate2::write::GzEncoder; // from `flate2` package we are using this encoder
 use flate2::Compression; // compression package
 use std::env::args; // to accept the command line arguments
 use std::fs::File; // since we are working with the files
@@ -9,8 +9,8 @@ use std::io::BufReader; // to read the files
 use std::time::Instant; // here it will show how much time taken to compress the files 
 
 fn main() {
-    if args().len() != 3 { 
-        eprintln!("Usage: 'source' 'target' ");
+    if args().len() != 3 { // if you give less than 3 arg the program will get panic
+        eprintln!("Usage: 'source' 'target' "); // we use `eprintln` instead of `println` as eprintln is only for error and progress message. 
         return;
     }
     let mut input = BufReader::new(File::open(args().nth(1).unwrap()).unwrap());
