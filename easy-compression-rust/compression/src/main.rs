9766f7a38e454e33e21e7ea8fbfda17d418b3d18
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-18 13:05:23 +0200

    Added comments

--- easy-compression-rust/compression/src/main.rs
+++ easy-compression-rust/compression/src/main.rs
@@ -1,15 +1,15 @@
 extern crate flate2; //import the flate2 package
 
 use flate2::write::GzEncoder; // from flate2 package we are using this encoder
-use flate2::Compression; // compression pacakge
+use flate2::Compression; // compression package
 use std::env::args; // to accept the command line arguments
-use std::fs::File; //since we are working with the files
-use std::io::copy;
-use std::io::BufReader;
-use std::time::Instant;
+use std::fs::File; // since we are working with the files
+use std::io::copy; // to copy the file
+use std::io::BufReader; // to read the files
+use std::time::Instant; // here it will show how much time taken to compress the files 
 
 fn main() {
-    if args().len() != 3 {
+    if args().len() != 3 { 
         eprintln!("Usage: 'source' 'target' ");
         return;
     }
