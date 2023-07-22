408efa5764404b9cda870077e3939b2efd183b36
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-23 01:56:55 +0200

    New

--- easy-decompression-rust/decompression/src/main.rs
+++ easy-decompression-rust/decompression/src/main.rs
@@ -15,9 +15,15 @@ fn real_main() -> i32 {
     };
 
     let fname = std::path::Path::new(&*args[1]);
-    let file = File::open(&fname).unwrap();
+    let file = File::open(&fname).unwrap(); // we open up the file to read
 
-    let _l_zip = ZipArchive::new(file);
+    let archive = ZipArchive::new(file).unwrap(); // Here we are gonna use the zip reader to read the data from the files
+
+    // To go over all the content
+
+    for i in 0..archive.len() {
+        let file = archive.by_index(i).unwrap();
+    }
 
     return 0;
 }
