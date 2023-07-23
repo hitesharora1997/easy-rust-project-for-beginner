ee9e53904973de4374df64df3bfe21876c745b6c
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-07-23 02:27:57 +0200

    After fixing some stuff

--- easy-decompression-rust/decompression/src/main.rs
+++ easy-decompression-rust/decompression/src/main.rs
@@ -17,12 +17,12 @@ fn real_main() -> i32 {
     let fname = std::path::Path::new(&*args[1]);
     let file = File::open(&fname).unwrap(); // we open up the file to read
 
-    let archive = ZipArchive::new(file).unwrap(); // Here we are gonna use the zip reader to read the data from the files
+    let mut archive = ZipArchive::new(file).unwrap(); // Here we are gonna use the zip reader to read the data from the files
 
     // To go over all the content
 
     for i in 0..archive.len() {
-        let file = archive.by_index(i).unwrap();
+        let mut file = archive.by_index(i);
     }
 
     return 0;
