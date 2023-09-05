d64b991bdad2af05f7c2d350ef733e21b7090d5b
Hitesh Arora <hitesh.arora1997@gmail.com>
2023-09-05 15:59:16 +0200

    new

--- write-json-rust/write-json/src/main.rs
+++ write-json-rust/write-json/src/main.rs
@@ -12,4 +12,23 @@ struct Article {
     paragragh: Vec<Paragraph>,
 }
 
-fn main() {}
+fn main() {
+    let article: Article = Article {
+        article: String::from("How to write to json in Rust"),
+        author: String::from("Hitesh"),
+        paragragh: vec![
+            Paragraph {
+                name: String::from("first sentence"),
+            },
+            Paragraph {
+                name: String::from("body of the paragragh"),
+            },
+            Paragraph {
+                name: String::from("last paragraph"),
+            },
+        ],
+    };
+
+    let json = serde_json::to_string(&article).unwrap();
+    println!("the json is {:?}", json);
+}
